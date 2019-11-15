33345d008b6d8964a7e2d0b654d208d533cf0f1d
Dimitris Provatas <james.provatas@gmail.com>
2019-11-15 21:27:34 +0200

    Main framework in place
    The main framework is placed. Now begins the process of laying down the basic mechanics of the engine (spawning, actual rendering etc)

--- classes/engine.py
+++ classes/engine.py
@@ -27,52 +27,66 @@ class engine():
 
         #glRotatef(25, 5, 5, 5)
 
+        # Cube Spawn
+
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
+            self.main(self)
+
+    def main(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            # Key inputs
+            elif event.type == KEYDOWN:
+                #Quit
+                if event.key == K_ESCAPE:
                     pygame.quit()
                     quit()
-                # Key inputs
-                elif event.type == KEYDOWN:
-                    #Quit
-                    if event.key == K_ESCAPE:
-                        pygame.quit()
-                        quit()
+
+                #Left
+                if event.key == pygame.K_a:
+                    glTranslatef(0.1, 0, 0)
+                #Right
+                if event.key == pygame.K_d:
+                    glTranslatef(-0.1, 0, 0)
+                #Up
+                if event.key == pygame.K_w:
+                    glTranslatef(0, -0.1, 0)
+                #Down
+                if event.key == pygame.K_s:
+                    glTranslatef(0, 0.1, 0)
+
+                #Rotate Left
+                if event.key == pygame.K_q:
+                    glRotatef(5, 0, 1, 0)
+                #Rotate Right
+                if event.key == pygame.K_e:
+                    glRotatef(-5, 0, 1, 0)
+
+            # Mouse inputs
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                #Mouse wheel
+
+                #Upwards
+                if event.button == 4:
+                    glTranslatef(0, 0, -0.5)
+                #Downwards
+                if event.button == 5:
+                    glTranslatef(0, 0, 0.5)
                     
-                    #Left
-                    if event.key == pygame.K_a:
-                        glTranslatef(-0.1, 0, 0)
-                    #Right
-                    if event.key == pygame.K_d:
-                        glTranslatef(0.1, 0, 0)
-                    #Up
-                    if event.key == pygame.K_w:
-                        glTranslatef(0, 0.1, 0)
-                    #Down
-                    if event.key == pygame.K_s:
-                        glTranslatef(0, -0.1, 0)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
-                    #Rotate Left
-                    if event.key == pygame.K_q:
-                        glRotatef(-5, 0, 1, 0)
-                    #Rotate Right
-                    if event.key == pygame.K_e:
-                        glRotatef(5, 0, 1, 0)
-                    
-                # Mouse inputs
-                elif event.type == pygame.MOUSEBUTTONDOWN:
-                    #Mouse wheel
-
-                    #Upwards
-                    if event.button == 4:
-                        glTranslatef(0, 0, -0.5)
-                    #Downwards
-                    if event.button == 5:
-                        glTranslatef(0, 0, 0.5)
-
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-
+        for x in range(10):
             Cube.__init__(Cube, 5, 3, 7)
 
-            pygame.display.flip()
-            pygame.time.wait(1) # in milisecs
+        # Camera Works
+        camera = glGetDoublev(GL_MODELVIEW_MATRIX)
+        
+        camera_x = camera[3][0]
+        camera_y = camera[3][1]
+        camera_z = camera[3][2]
+        #print("x= ", camera_x, ", y=", camera_y, ", z= ", camera_x)
+
+        pygame.display.flip()
+        pygame.time.wait(1) # in milisecs
